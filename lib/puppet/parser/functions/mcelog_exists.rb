2f07b7b60c4c32252dc9019d9be74bc93b8f9386
Peterclaver Emedom <pemedom1_ms@rocky-pup2k15mas-01.us.umuc.edu>
2016-09-18 21:03:29 -0400

    Creating the first puppet function

--- lib/puppet/parser/functions/mcelog_exists.rb
+++ lib/puppet/parser/functions/mcelog_exists.rb
@@ -1,6 +1,9 @@
+
+#require 'mcelog'
+
 module Puppet::Parser::Functions
   newfunction(:mcelog_exists, :type => :rvalue) do |arg|
-    if File.exist? /usr/sbin/mcelog'
+    if File.exist?'/usr/sbin/mcelog'
       0
      else
       1
